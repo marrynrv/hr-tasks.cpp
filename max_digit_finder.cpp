7c726f3bd1621633bc83f176fad009c2217c712e
Maryam <mnuriyeva96@gmail.com>
2023-08-18 12:23:13 +0400

    max_digit_finder added one more solution

--- max_digit_finder.cpp
+++ max_digit_finder.cpp
@@ -16,6 +16,15 @@ result=0;
  }
  cout<<result<<endl;
  */
+/*  int n,smallest=0, largest=0;
+  cin>>n;
+  while(n > 0){
+     int r = n % 10;
+     largest = max(r,largest);
+     smallest = min(r,smallest);
+     n = n/10;
+  }
+  cout<<largest;   */
 int n,result;
 cin>>n;
 result=0;
