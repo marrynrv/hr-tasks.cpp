0a698fc49760b54053e505363fc397ca29095a55
Maryam <mnuriyeva96@gmail.com>
2023-08-20 22:13:05 +0400

    added comments to lcm_to_n.cpp

--- lcm_to_n.cpp
+++ lcm_to_n.cpp
@@ -28,22 +28,27 @@ int main() {
 using namespace std;
 
 int main() {
+    // Read the value of N from the user
     int n;
     cin >> n;
 
+    // Initialize the result with 1, as it is the identity element for multiplication
     int result = 1;
 
+    // Iterate through numbers from 1 to N
     for (int i = 1; i <= n; i++) {
-        int a = result;
-        int b = i;
+        int a = result;  // Store the current LCM in 'a'
+        int b = i;       // 'b' represents the current number in the iteration
 
+        // Find the LCM of 'a' and 'b' iteratively
         while (a % b != 0) {
-            a = a + result;
+            a = a + result;  // Add the previous LCM to 'a'
         }
 
-        result = a;
+        result = a;  // Update the LCM to the calculated value
     }
 
+    // Print the final calculated LCM of all integers from 1 to N
     cout << result << endl;
 
     return 0;
