084e85dbbbb962fe132aedcd380289dd2c2c5bb9
Maryam <mnuriyeva96@gmail.com>
2023-08-31 16:24:18 +0400

    adding comments to 68 task

--- 2.cyclic-constructions/perfect-nums.cpp
+++ 2.cyclic-constructions/perfect-nums.cpp
@@ -7,16 +7,16 @@ int main() {
     cin >> N;
 
     for (i = 1; i <= N; ++i) {
-        sumOfDivisors = 0;
+        sumOfDivisors = 0;      // Summarizing divisors for each i number
 
         for (j = 1; j < i; ++j) {
-            if (i % j == 0) {
-                sumOfDivisors += j;
+            if (i % j == 0) {   // Finding divisors of i number
+                sumOfDivisors += j; // Summarizing divisors
             }
         }
 
-        if (sumOfDivisors == i) {
-            cout << i << " ";
+        if (sumOfDivisors == i) {   // If sum of divisors is equal to number
+            cout << i << " ";      // Print number
         }
     }
 
